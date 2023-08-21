d5bdd294caae78b3fab6b2793ad9d6daa4836bf3
Snehal Patel <48606159+pate1431@users.noreply.github.com>
2023-08-21 09:09:59 -0400

    Add files via upload
    Pocos Files

---
+++ Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocos
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public ISet<Employee> Employees { get; set;}
+
+
+        public override string ToString()
+        {
+            return $"Department Id:{Id} \t Department Name:{DeptName}";
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocos
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }    
+        public string LastName { get; set; }
+
+        public Department Dept{ get; set; }
+
+        public override string ToString()
+        {
+            List<Department> departmentList = new List<Department>();
+            
+            return $"Employee Id:{Id} \t First Name:{FirstName} \t Last Name:{LastName} \t Dept:{Dept.Id} ";
+        }
+    }
+}
